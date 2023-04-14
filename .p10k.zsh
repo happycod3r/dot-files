b22a8cf1a784c19fd630cbe4bcd92b86225f1e04
happycod3r <happy2code0101@gmail.com>
2023-04-14 17:07:16 -0400

    Update .p10k.zsh...

--- .p10k.zsh
+++ .p10k.zsh
@@ -189,7 +189,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=200
   # Custom icon.
   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🗀'
 
